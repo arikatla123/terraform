e9a896446cf5bf02df301f8b48fa9c85ab093e63
arikatla123 <60966057+arikatla123@users.noreply.github.com>
2020-08-08 21:53:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,7 @@
-
-resource "aws_instance" "mysample" {
-  ami           = "ami-066df92ac6f03efca"
+resource "aws_instance" "example" {
+  ami           = "ami-021bb9f371690f97a"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "Subba-terraform"
+   tags = {
+    Name = "terraform instance"
   }
-
 }
