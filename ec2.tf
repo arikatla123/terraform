f9155b395f8b95c5dda007705016aacdac9411c8
arikatla123 <60966057+arikatla123@users.noreply.github.com>
2020-08-08 21:38:40 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-  region     = "us-west-1"
-  access_key = "AKIA4X4UVQ4FHENUSR2A"
-  secret_key = "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"
-}
-
-
 
 resource "aws_instance" "mysample" {
   ami           = "ami-066df92ac6f03efca"
