229a279e916df787b8b5e2121c47a7e6e23804d7
arikatla123 <60966057+arikatla123@users.noreply.github.com>
2020-08-09 14:19:42 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,30 +8,3 @@ resource "aws_instance" "example" {
   }
 
 }
-
-resource "aws_eip" "lb" {
-  instance = "${aws_instance.example.id}"
-  vpc      = true
-tags = {
-    Name = "custmyeip"
-  }
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "subba-test-s3-terraform-bucket"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-
-  tags {
-    Name = "my-test-s3-terraform-bucket"
-  }
-}
-
-resource "aws_s3_bucket_object" "file_upload" {
-  bucket = "subba-test-s3-terraform-bucket"
-  key    = "my_bucket_key"
-  source = "terraform.tfstate"
-
-}
