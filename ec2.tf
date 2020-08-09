7440e1717e07413cb0fb650c816f5a5ae2f21c7b
arikatla123 <60966057+arikatla123@users.noreply.github.com>
2020-08-09 14:17:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,37 @@
 
-resource "aws_instance" "myfirst-terrafrom" {
+resource "aws_instance" "example" {
   ami           = "ami-021bb9f371690f97a"
   instance_type = "t2.micro"
-   tags = {
-    Name = "terraform-instance"
+
+  tags = {
+    Name = "terraforminstance"
   }
+
+}
+
+resource "aws_eip" "lb" {
+  instance = "${aws_instance.example.id}"
+  vpc      = true
+tags = {
+    Name = "custmyeip"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "subba-test-s3-terraform-bucket"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+
+  tags {
+    Name = "my-test-s3-terraform-bucket"
+  }
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = "subba-test-s3-terraform-bucket"
+  key    = "my_bucket_key"
+  source = "terraform.tfstate"
+
 }
