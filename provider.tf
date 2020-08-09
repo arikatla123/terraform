1a11382ffb4539c6a918167a76bd79c6cd55b2af
arikatla123 <60966057+arikatla123@users.noreply.github.com>
2020-08-09 14:09:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-1"
-  access_key = "AKIA4X4UVQ4FHENUSR2A"
-  secret_key = "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"
+  access_key = "AKIAJEA6YFYPQPDN7MJA"
+  secret_key = "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"
 }
