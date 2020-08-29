e6f2e4a0171ce4b235531619ff81cdb5b071c4f4
arikatla123 <60966057+arikatla123@users.noreply.github.com>
2020-08-29 19:58:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,18 @@
-
-resource "aws_instance" "example" {
-  ami           = "ami-021bb9f371690f97a"
+resource "aws_instance" "MyFirstEc2instance_from_terraform" {
+  ami = "ami-05655c267c89566dd"
   instance_type = "t2.micro"
-
   tags = {
-    Name = "terraforminstance"
+      Name = "EC2started_from_console_from_terraform"
+          key_name = "ec2demo_1"
   }
 
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd.service
+                systemctl enable httpd.service
+                echo "Hi Friend , I am $(hostname -f) hosted by Terraform" > /var/www/html/index.html
+                EOF
 }
+
